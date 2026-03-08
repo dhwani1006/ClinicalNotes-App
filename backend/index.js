5901b45c0d071b0f97bebfceb6488ff543a2b670
dhwani1006 <thakkardhwani1998@gmail.com>
2026-03-08 17:46:44 +0530

    fix render port

--- backend/index.js
+++ backend/index.js
@@ -111,6 +111,8 @@ app.use("/api/notes", notesRoutes);
 app.use("/api/users", usersRoutes); // 👈 NEW
 
 // Start server
-app.listen(5000, "0.0.0.0", () => {
-  console.log("Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on port ${PORT}`);
 });
