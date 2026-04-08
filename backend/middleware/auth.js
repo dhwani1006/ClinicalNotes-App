a30ef976bd6f16c18df6a9d7e60950bfa71b8d2e
dhwani1006 <thakkardhwani1998@gmail.com>
2026-04-09 03:35:24 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -22,24 +22,3 @@ export default function auth(req, res, next) {
 
 
 
-// import jwt from "jsonwebtoken";
-
-// export default function auth(req, res, next) {
-//   const header = req.headers.authorization;
-
-//   // Expect: "Authorization: Bearer <token>"
-//   if (!header || !header.startsWith("Bearer ")) {
-//     return res.status(401).json({ message: "No token provided" });
-//   }
-
-//   const token = header.split(" ")[1];
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     // decoded = { id, name, email, iat, exp }
-//     req.user = decoded; // 👈 attach user info to request
-//     next();
-//   } catch (err) {
-//     return res.status(401).json({ message: "Invalid or expired token" });
-//   }
-// }
